bf49d52ac38c4b0d6d74c19d1ba0964d56d47e32
ser <seregaschukin96@mail.ru>
2021-02-15 18:47:11 +0300

    Тест

---
+++ hello.py
@@ -0,0 +1,14 @@
+duration = int(input('Введите время в секундах: '))
+minute = 60
+hour = 60 * 60
+day = 60 * 60 * 24
+
+if duration < minute:
+    print(duration, 'сек')
+elif minute <= duration < hour:
+    print(duration // minute, 'мин', duration % minute, 'сек')
+elif hour <= duration < day:
+    print(duration // hour,  'час', duration // minute % minute, 'мин', duration % minute, 'сек' )
+else:
+    print(duration // day, 'дн', duration % day // hour, 'час', duration // minute % minute, 'мин', duration % minute, 'сек')
+
