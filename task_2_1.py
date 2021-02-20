e469cba5cae20a37291abc82c53670e9611a9234
ser <seregaschukin96@mail.ru>
2021-02-20 19:18:05 +0300

    Дз №2

---
+++ task_2_1.py
@@ -0,0 +1,15 @@
+# 1. Выяснить тип результата выражений:
+# 15 * 3
+# 15 / 3
+# 15 // 2
+# 15 ** 2
+
+a, b, c, d = 15 * 3, 15 / 3, 15 // 2, 15 ** 2
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
