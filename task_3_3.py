ed0054fc22228570079089f4b14b023910fddfc8
Seregaschukin <78037157+Seregaschukin@users.noreply.github.com>
2021-02-26 23:33:46 +0300

    Update task_3_3.py

--- task_3_3.py
+++ task_3_3.py
@@ -13,7 +13,19 @@
 # Подумайте: полезен ли будет вам оператор распаковки?
 # Сможете ли вы вернуть отсортированный по ключам словарь?
 
+def thesaurus(*args):
+    thesaurus_dicts = dict()
+    for vals in args:
+        key = vals[:1]
+        if key in thesaurus_dicts.keys():
+            thesaurus_dicts[key].append(vals)
+        else:
+            thesaurus_dicts[key] = [vals]
+    #return thesaurus_dicts #без сортировки
 
+    sort_dict = dict()
+    for key in sorted(thesaurus_dicts.keys()):
+        sort_dict[key] = thesaurus_dicts[key]
+    return sort_dict
 
-
-
+print(thesaurus("Иван", "Мария", "Петр", "Илья", "Федя", "Антон", "Петр", "Илья"))
