f36dff77b1fc7e8c0cbf53428db6f777ab6c6d89
Seregaschukin <78037157+Seregaschukin@users.noreply.github.com>
2021-02-26 22:00:16 +0300

    Update task_3_5.py

--- task_3_5.py
+++ task_3_5.py
@@ -11,3 +11,28 @@
 # ["лес завтра зеленый", "город вчера веселый"]
 #
 # Документировать код функции.
+from random import choice
+
+def get_jokes(max_jokes, nouns, adverbs, adjectives):
+    """
+    :param max_jokes: Number of jokes
+    :param nouns:the first word of the joke
+    :param adverbs:the second word of the joke
+    :param adjectives:the third word of the joke
+    :return: jokes
+    """
+    #Возьмем из каждого списка по 1 слову при помощи ранее импортированного модуля, выйдем из вайл по условию
+    n = 0
+    jokes = []
+    while n < max_jokes:
+        jokes.append(f'{choice(nouns)} {choice(adverbs)} {choice(adjectives)}')
+        n += 1
+    return jokes
+
+nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+
+max_jokes = int(input('Введите количество шуток: '))
+get_jokes(max_jokes, nouns, adverbs, adjectives)
+print(get_jokes(max_jokes, nouns, adverbs, adjectives))
