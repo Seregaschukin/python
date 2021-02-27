e77fd044362db57235ae6aeb1acd1007732d9e12
ser <seregaschukin96@mail.ru>
2021-02-27 19:22:51 +0300

    ДЗ №4

---
+++ task_4_2.py
@@ -0,0 +1,45 @@
+#2. Написать функцию currency_rates(), принимающую в качестве аргумента код валюты (USD, EUR, ...)
+# и возвращающую курс этой валюты по отношению к рублю. Использовать библиотеку requests.
+# В качестве API можно использовать http://www.cbr.ru/scripts/XML_daily.asp.
+# Рекомендация: выполнить предварительно запрос к API в обычном браузере, посмотреть содержимое ответа.
+# Можно ли, используя только методы класса str, решить поставленную задачу?
+# Функция должна возвращать результат числового типа, например float.
+# Подумайте: есть ли смысл для работы с денежными величинами использовать вместо float тип Decimal?
+# Сильно ли усложняется код функции при этом? Если в качестве аргумента передали код валюты,
+# которого нет в ответе, вернуть None. Можно ли сделать работу функции не зависящей от того,
+# в каком регистре был передан аргумент?
+# В качестве примера выведите курсы доллара и евро.
+
+
+from requests import get, utils
+
+def currency_rates(currensy):
+    response = get('http://www.cbr.ru/scripts/XML_daily.asp')
+    encodings = utils.get_encoding_from_headers(response.headers)
+    content = response.content.decode(encoding=encodings)
+    #print(type(content))
+    find_curr = content.find(currensy)
+    interval = find_curr + 200
+    new_str = content[find_curr:interval]
+    find_new_str = new_str.find('<Value>')
+    start_find = find_new_str + 7
+    finish_find = start_find + 7
+    print_find_currensy = new_str[start_find:finish_find]  # 1723:1730
+    #print(f'{print_find_currensy} RUB за 1 единицу валюты')
+    return print_find_currensy
+    # cur_rub = 1
+    # rate = cur_rub / print_find_currensy
+    # return rate
+currensy = input('Введите индекс валюты: ')
+print(f'{currency_rates(currensy)} RUB за 1 единицу {currensy}')
+
+
+
+
+
+
+
+
+
+
+
