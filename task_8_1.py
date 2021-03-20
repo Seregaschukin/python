bab7b0e5585c7c9aa92a18d12bea405c9fe85ed1
Seregaschukin <78037157+Seregaschukin@users.noreply.github.com>
2021-03-20 19:55:38 +0300

    Update task_8_1.py

--- task_8_1.py
+++ task_8_1.py
@@ -21,3 +21,36 @@
 #
 # Примечание: подумайте о возможных ошибках в адресе и постарайтесь учесть их в регулярном выражении;
 # имеет ли смысл в данном случае использовать функцию re.compile()?
+
+import re
+
+#(\w[a-z'._+-]*)\@(\w[a-z'.-]*) - не плохой
+
+def email_parse(email):
+# с ValueError
+    email_pars_new = "(\w[a-z'._+-]*)\@((\w[a-z'.-]*)\.\w+)"
+    huhd = re.fullmatch(email_pars_new, email)
+    if huhd is None:
+        raise ValueError
+    huhd = huhd.groups()
+    return {'username': huhd[0], 'domain': huhd[1]}
+
+    #без .groups()
+    #return {'username': huhd[1], 'domain': huhd[2]}
+
+# без ValueError
+# def email_parse(email):
+#     parse_dict = {}
+#     email_pars_new = re.compile(r"(\w[a-z'._+-]*)\@((\w[a-z'.-]*)\.\w+)")
+#     huhd = email_pars_new.findall(email)
+#     for i in huhd:
+#         parse_dict['username'] = i[0]
+#         parse_dict['domain'] = i[1]
+#     return parse_dict
+
+print(email_parse('someone@geekbrains.ru'))
+
+#для проверки
+#print(email_parse('someone@geekbrainsru'))
+
+print(email_parse('sere.gas-chukin@mail.ru'))
